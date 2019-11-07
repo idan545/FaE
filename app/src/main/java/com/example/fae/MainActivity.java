5a7dd05325b878392312a149ae540e5254f4c106
Idan545 <idancohen545@gmail.com>
2019-11-07 09:23:21 +0200

    sad

---
+++ app/src/main/java/com/example/fae/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.fae;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById (R.id.btn);
+    }
+
+    public void firebs1(View view) {
+
+    }
+}
