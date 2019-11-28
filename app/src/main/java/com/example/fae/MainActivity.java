409aa99f5df3a1ab75ebcc1c9b100d46f30c119b
liemb <lb6031@bs.amalnet.k12.il>
2019-11-28 09:18:25 +0200

    New Steps 28/11/19

--- app/src/main/java/com/example/fae/MainActivity.java
+++ app/src/main/java/com/example/fae/MainActivity.java
@@ -1,46 +1,30 @@
 package com.example.fae;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
+    EditText pNumber;
+    Intent t;
+    FirebaseAuth fbA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = (Button) findViewById (R.id.btn);
+        btn = (Button) findViewById(R.id.btn);
+        pNumber = (EditText) findViewById(R.id.editText);
+        fbA = FirebaseAuth.getInstance();
     }
 
-    public class Student {
+    public void createAccount() {
 
-        private int GradeClass;
-        private int StuClass;
-        private String StuName;
-        private String StuID;
-
-        public Student(int GradeClass, int StuClass, String StuName, String StuID) {
-            this.GradeClass = GradeClass;
-            this.StuClass = StuClass;
-            this.StuName = StuName;
-            this.StuID = StuID;
-        }
     }
-    public class StuGrade {
-
-        private String StuID;
-        private String ClassSubject;
-        private String GradeType;
-        private int FinalGrade;
-
-        public StuGrade(String StuID, String ClassSubject, String GradeType, int FinalGrade) {
-            this.StuID = StuID;
-            this.ClassSubject = ClassSubject;
-            this.GradeType = GradeType;
-            this.FinalGrade = FinalGrade;
-        }
-    }
-    }
+}
